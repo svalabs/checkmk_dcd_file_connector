cc4bd8c3b82ef9442574c4f018a1ddef86bce405
Niko Wenselowski <niko.wenselowski@sva.de>
2020-03-05 19:04:38 +0100

    We don't need all these imports.

--- lib/python/csvconnector/helper.py
+++ lib/python/csvconnector/helper.py
@@ -17,10 +17,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-
-from typing import (  # pylint: disable=unused-import
-    Dict, List, Tuple,
-)
+from typing import Dict  # pylint: disable=unused-import
 
 
 def normalize_hostname(hostname):
