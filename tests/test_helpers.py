f5cfd9b08a768f5e67bded1f123ae5aed6992eb0
Niko Wenselowski <niko.wenselowski@sva.de>
2020-08-05 10:31:28 +0200

    Make sure values aren't changed.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -9,11 +9,15 @@ def test_getting_host_label():
         hostname_field: 'testhost',
         'LaBeL1': 1,
         'label2': 2,
+        'Dont_Lowercase_Value': 'DevOps',
+        'dont_lowercase_value2': 'ITIL',
     }
 
     expected_label = {
         'label1': 1,  # key should be lowercased
         'label2': 2,
+        'dont_lowercase_value': 'DevOps',
+        'dont_lowercase_value2': 'ITIL',
     }
 
     assert expected_label == helper.get_host_label(host, hostname_field)
