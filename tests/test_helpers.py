2196e9025d95f7d9f6b97b1880a86488bc6b7c89
Niko Wenselowski <niko.wenselowski@sva.de>
2020-08-05 10:47:30 +0200

    Values should not be changed.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -22,17 +22,16 @@ def test_getting_host_label(hostname_field):
     assert expected_label == helper.get_host_label(host, hostname_field)
 
 
-@pytest.mark.parametrize("key, expected_key, value, expected_value",
-    ('LaBeL1', 1, 'label1', 1),  # key should be lowercased
-    ('label2', 2, 'label2', 2),
-    ('Dont_Lowercase_Value', 'DevOps', 'dont_lowercase_value', 'DevOps'),
-    ('dont_lowercase_value2', 'ITIL', 'dont_lowercase_value2', 'ITIL'),
-
-)
-def test_getting_host_label_transformation(hostname_field, key, expected_key, value, expected_value):
+@pytest.mark.parametrize("key, expected_key, value", (
+    ('LaBeL1', 'label1', 1),  # key should be lowercased
+    ('label2', 'label2', 2),
+    ('Dont_Lowercase_Value', 'dont_lowercase_value', 'DevOps'),
+    ('dont_lowercase_value2', 'dont_lowercase_value2', 'ITIL'),
+))
+def test_getting_host_label_transformation(hostname_field, key, expected_key, value):
     host = {hostname_field: 'testhost', key: value}
 
-    expected_label = {expected_key, expected_label}
+    expected_label = {expected_key: value}
 
     assert expected_label == helper.get_host_label(host, hostname_field)
 
