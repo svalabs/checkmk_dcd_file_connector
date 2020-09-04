bc825c563b92743cdcb6c5b5155771b6ed35acd0
Niko Wenselowski <niko.wenselowski@sva.de>
2020-09-04 09:59:39 +0200

    Convert to CMK 1.7 plugin format.

--- web/plugins/wato/csvconnector.py
+++ web/plugins/wato/csvconnector.py
@@ -17,4 +17,81 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-import csvconnector  # NOQA
+from cmk.gui.cee.plugins.wato.dcd import (  # noqa: F401 # pylint: disable=unused-import
+    connector_parameters_registry, ConnectorParameters,
+)
+
+from cmk.gui.exceptions import MKUserError
+
+from cmk.gui.i18n import _
+
+from cmk.gui.plugins.wato import FullPathFolderChoice
+
+from cmk.gui.valuespec import (
+    Age,
+    Filename,
+    Dictionary,
+    ListOfStrings,
+    RegExpUnicode,
+)
+
+
+@connector_parameters_registry.register
+class CSVConnectorParameters(ConnectorParameters):
+    def name(self):
+        return "csvconnector"
+
+    def title(self):
+        return _("CSV import")
+
+    def description(self):
+        return _("Connector for importing data from a CSV file.")
+
+    def valuespec(self):
+        return Dictionary(
+            elements=[
+                ("interval", Age(
+                    title=_("Sync interval"),
+                    minvalue=1,
+                    default_value=60,
+                )),
+                ("path", Filename(
+                    title=_("Path of to the CSV file to import."),
+                    help=_("This is the path to the CSV file. "
+                           "The first column of the file is assumed to contain the hostname."),
+                    allow_empty=False,
+                    validate=self.validate_csv,
+                )),
+                ("folder", FullPathFolderChoice(
+                    title=_("Create hosts in"),
+                    help=_("All hosts created by this connection will be "
+                           "placed in this folder. You are free to move the "
+                           "host to another folder after creation."),
+                )),
+                ("host_filters", ListOfStrings(
+                    title=_("Only add matching hosts"),
+                    help=_(
+                        "Only care about hosts with names that match one of these "
+                        "regular expressions."),
+                    orientation="horizontal",
+                    valuespec=RegExpUnicode(mode=RegExpUnicode.prefix,),
+                )),
+                ("host_overtake_filters", ListOfStrings(
+                    title=_("Take over existing hosts"),
+                    help=_(
+                        "Take over already existing hosts with names that "
+                        "match one of these regular expressions. This will not"
+                        "take over hosts handled by foreign connections or "
+                        "plugins. Hosts that have been took over will be "
+                        "deleted once they vanish from the import file."),
+                    orientation="horizontal",
+                    valuespec=RegExpUnicode(mode=RegExpUnicode.prefix,),
+                )),
+            ],
+            optional_keys=["host_filters", "host_overtake_filters"],
+        )
+
+    @staticmethod
+    def validate_csv(filename, varprefix):
+        if not os.path.isfile(filename):
+            raise MKUserError(varprefix, "No file %r" % filename)
