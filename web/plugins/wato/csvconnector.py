953c5b2ad1e679b17e726ef66882e0ced104c14b
Niko Wenselowski <niko.wenselowski@sva.de>
2020-11-24 12:40:47 +0100

    Make the import more specific

--- web/plugins/wato/csvconnector.py
+++ web/plugins/wato/csvconnector.py
@@ -17,7 +17,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-import os
+import os.path
 
 from cmk.gui.cee.plugins.wato.dcd import (  # noqa: F401 # pylint: disable=unused-import
     connector_parameters_registry, ConnectorParameters,
