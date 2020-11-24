cb4bc6a6f2cbd98dfc1f5e5cdf16bacf9e62e06f
Niko Wenselowski <niko.wenselowski@sva.de>
2020-11-24 12:33:54 +0100

    Show that this are our plans

--- lib/python/csvconnector.py
+++ lib/python/csvconnector.py
@@ -399,7 +399,7 @@ class CSVConnector(Connector):
         hosts_to_delete = list(set(hosts_managed_by_plugin) - cmdb_hostnames)
 
         self._logger.info(
-            "Hosts: %i to create, %i to modify, %i to delete",
+            "Planned host actions: %i to create, %i to modify, %i to delete",
             len(hosts_to_create),
             len(hosts_to_modify),
             len(hosts_to_delete),
