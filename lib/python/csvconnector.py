9f48d55984d206593d7e0c4334e3efcc90afb98c
Niko Wenselowski <niko.wenselowski@sva.de>
2021-02-10 16:25:16 +0100

    Use the new copyright header

--- lib/python/csvconnector.py
+++ lib/python/csvconnector.py
@@ -1,21 +1,18 @@
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
 
-# Copyright (C) 2020  Niko Wenselowski <niko.wenselowski@sva.de>
+# +------------------------------------------------------------+
+# |                                                            |
+# |             | |             | |            | |             |
+# |          ___| |__   ___  ___| | ___ __ ___ | | __          |
+# |         / __| '_ \ / _ \/ __| |/ / '_ ` _ \| |/ /          |
+# |        | (__| | | |  __/ (__|   <| | | | | |   <           |
+# |         \___|_| |_|\___|\___|_|\_\_| |_| |_|_|\_\          |
+# |                                   custom code by SVA       |
+# |                                                            |
+# +------------------------------------------------------------+
+#
+# Copyright (C) 2021  Niko Wenselowski <niko.wenselowski@sva.de>
 #                     for SVA System Vertrieb Alexander GmbH
-
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 from __future__ import absolute_import
 
