85104c68d6ef9650457292761fd906f24f3c2fef
Niko Wenselowski <niko.wenselowski@sva.de>
2021-06-15 18:37:19 +0200

    Add chunking tests

---
+++ tests/test_chunking.py
@@ -0,0 +1,26 @@
+import pytest
+import csvconnector
+
+
+def test_chunking(chunk_size=3):
+    for chunk in csvconnector.chunks(range(10), chunk_size):
+        assert len(chunk) == chunk_size
+
+
+def test_chunking_fills_with_none():
+    chunk_size = 3
+    original_iter = list(range(10))
+    assert len(original_iter) % 3 != 0
+
+    for index, chunk in enumerate(csvconnector.chunks(original_iter, chunk_size)):
+        print(chunk)
+        if index != chunk_size:
+            assert len([x for x in chunk if x is not None]) == chunk_size
+            continue  # No further checks
+
+        chunk_length = len(chunk)
+        empty_elements = [x for x in chunk if x is None]
+        assert empty_elements
+
+        filled_elements = [x for x in chunk if x is not None]
+        assert chunk_size == len(filled_elements + empty_elements)
