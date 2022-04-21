86cd9a3d95c350fdeeefead25a009cbb4dfa68c9
Niko Wenselowski <niko.wenselowski@sva.de>
2022-04-21 16:39:40 +0200

    Simplify tests

--- tests/test_lowercase_import.py
+++ tests/test_lowercase_import.py
@@ -40,58 +40,52 @@ def import_path():
 
 
 @pytest.fixture
-def importer(import_path):
+def fakeImporter(import_path):
     return FakeImporter(import_path)
 
 
-def test_path_is_not_changed(importer, import_path):
-    lowercaseImporter = LowercaseImporter(importer)
+@pytest.fixture
+def importer(fakeImporter):
+    return LowercaseImporter(fakeImporter)
 
-    assert import_path == lowercaseImporter.filepath
+
+def test_path_is_not_changed(importer, import_path):
+    assert import_path == importer.filepath
 
 
 def test_lowercasing_hostname_field(importer):
-    lowercaseImporter = LowercaseImporter(importer)
-    lowercaseImporter.import_hosts()
+    importer.import_hosts()
 
-    assert lowercaseImporter.hostname_field.islower()
+    assert importer.hostname_field.islower()
 
 
 def test_lowercasing_hostname_field_only_if_filled(importer):
-    lowercaseImporter = LowercaseImporter(importer)
-
     # No import happens
-    assert lowercaseImporter.hostname_field is None
+    assert importer.hostname_field is None
 
 
 def test_lowercasing_fields(importer):
-    lowercaseImporter = LowercaseImporter(importer)
-    lowercaseImporter.import_hosts()
+    importer.import_hosts()
 
-    assert lowercaseImporter.fields
+    assert importer.fields
 
-    for fieldname in lowercaseImporter.fields:
+    for fieldname in importer.fields:
         assert fieldname.islower()
 
 
 def test_lowercasing_fields_only_if_filled(importer):
-    lowercaseImporter = LowercaseImporter(importer)
-
     # No import happens
-    assert lowercaseImporter.fields is None
+    assert importer.fields is None
 
 
 def test_lowercasing_hosts_only_if_filled(importer):
-    lowercaseImporter = LowercaseImporter(importer)
-
-    assert not lowercaseImporter.hosts
+    assert not importer.hosts
 
 
 def test_import_hosts(importer):
-    lowercaseImporter = LowercaseImporter(importer)
-    lowercaseImporter.import_hosts()
+    importer.import_hosts()
 
-    for host in lowercaseImporter.hosts:
+    for host in importer.hosts:
         for key, value in host.items():
             assert key.islower()
 
